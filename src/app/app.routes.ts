3e2d995135809446c122ef0e5be09b878f38efa9
Nathan Malbecq <la227315@student.helha.be>
2024-11-12 12:42:31 +0100

    Added navbar and creation of router-outlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './core/home/home.component';
+import {AuthComponent} from './core/auth/auth.component';
 
-export const routes: Routes = []
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'auth', component: AuthComponent },
+]
