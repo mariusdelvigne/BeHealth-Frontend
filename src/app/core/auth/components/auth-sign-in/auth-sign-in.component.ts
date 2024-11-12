dc15c44a011a591e00a9a4a90b9b53cc0770145c
RobinRHELHa <la229732@student.helha.be>
2024-11-12 23:48:07 +0100

    Add style to auth sign in form with animations

--- src/app/core/auth/components/auth-sign-in/auth-sign-in.component.ts
+++ src/app/core/auth/components/auth-sign-in/auth-sign-in.component.ts
@@ -2,15 +2,33 @@ import {Component} from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {AuthService} from '../../services/auth.service';
 import {Router} from '@angular/router';
+import {NgClass} from '@angular/common';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-auth-sign-in',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgClass
   ],
   templateUrl: './auth-sign-in.component.html',
-  styleUrl: './auth-sign-in.component.css'
+  styleUrl: './auth-sign-in.component.css',
+  animations: [
+    trigger('colorChange', [
+      state('grey', style({
+        backgroundColor: 'rgba(173, 181, 189, 0.3)',
+        border: '3px solid rgba(173, 181, 189, 0.5)',
+      })),
+      state('blue', style({
+        backgroundColor: 'rgba(13, 110, 253, 0.3)',
+        border: '3px solid rgba(13, 110, 253, 0.5)',
+      })),
+      transition('grey <=> blue', [
+        animate('1s ease-out')
+      ])
+    ])
+  ]
 })
 export class AuthSignInComponent {
   form: FormGroup = new FormGroup({
@@ -23,6 +41,11 @@ export class AuthSignInComponent {
   emitSignIn() {
     this._authService.signIn(this.form.value).subscribe({
       next: _ => this._router.navigate(['/']),
-    })
+    });
   }
+
+  get colorChange() {
+    return this.form.invalid ? 'grey' : 'blue';
+  }
+
 }
