169f129aea8f0682eeeefceec7020b978b5a8dd5
RobinRHELHa <la229732@student.helha.be>
2024-11-16 19:13:23 +0100

    Remove getAllStartingWith from UserSportService

--- src/app/shared/services/user-sport.service.ts
+++ src/app/shared/services/user-sport.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpParams} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {UserSportCreateCommand} from '../utils/user-sport-create-command';
 import {Observable} from 'rxjs';
 import {UserSportCreateOutput} from '../utils/user-sport-create-output';
@@ -18,14 +18,6 @@ export class UserSportService {
     let userId = this._authService.getId();
     return this._httpClient.post<UserSportCreateOutput>(UserSportService.URL + `/${userId}/sports`, userSportCreateCommand, {
       withCredentials: true,
-    });
-  }
-
-  public getAllStartingWith(text: string): Observable<any> {
-    const params = new HttpParams().set('text', text);
-    return this._httpClient.get<any>('http://localhost:5114/foods', {
-      withCredentials: true,
-      params: params,
     });
   }
 }
