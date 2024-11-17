1d1ba9d1c2545d9b5e83e38009c909790cae418b
Matteo Leroy <matteo.leroy2004@gmail.com>
2024-11-17 11:22:58 +0100

    Add text animation to home page

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,13 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('content') content!: ElementRef;
 
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.content.nativeElement.style.opacity = '1';
+      this.content.nativeElement.style.transform = 'translateX(0)';
+      }, 250); // Délai pour assurer que l'animation se joue après le rendu initial
+  }
 }
