ab981b4202955dfaffb0533614653ca56b0cc937
Marius Delvigne <marius.delvigne@gmail.com>
2024-11-20 09:38:36 +0100

    calcul BMi implementation

---
+++ src/app/features/bmiCalculator/components/bmi-calculator/bmi-calculator.component.ts
@@ -0,0 +1,59 @@
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+import {BmiCalculatorService} from '../../services/bmi-calculator.service';
+import {formatNumber} from '@angular/common';
+
+@Component({
+  selector: 'app-bmi-calculator',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule
+  ],
+  templateUrl: './bmi-calculator.component.html',
+  styleUrl: './bmi-calculator.component.css',
+  animations: [
+    trigger('colorChange', [
+      state('grey', style({
+        backgroundColor: 'rgba(173, 181, 189, 0.3)',
+        border: '3px solid rgba(173, 181, 189, 0.5)',
+      })),
+      state('blue', style({
+        backgroundColor: 'rgba(13, 110, 253, 0.3)',
+        border: '3px solid rgba(13, 110, 253, 0.5)',
+      })),
+      transition('grey <=> blue', [
+        animate('1s ease-out')
+      ])
+    ])
+  ]
+})
+export class BmiCalculatorComponent {
+  form: FormGroup = new FormGroup({
+    gender: new FormControl('Male', Validators.required),
+    age: new FormControl('', Validators.required),
+    heightInCm: new FormControl('', Validators.required),
+    weightInKg: new FormControl('', Validators.required),
+  });
+  resultBmi: number = 0.00;
+  resultBmiInterpretation: string = "";
+
+  constructor(private _bmiCalculatorService: BmiCalculatorService) {
+  }
+
+  get colorChange() {
+    return this.form.invalid ? 'grey' : 'blue';
+  }
+
+  calculBmi() {
+    this._bmiCalculatorService.calculBmi(this.form.value).subscribe({
+      next: (response) => {
+        this.resultBmi = Number(response.bmi.toFixed(2));
+        this.resultBmiInterpretation = response.bmiInterpretetion;
+      },
+      error: (error) => {
+        alert(error.message);
+      }
+    })
+  }
+}
---
+++ src/app/features/bmiCalculator/services/bmi-calculator.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {apis, environment} from '../../../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {BmiCalculatorCommand} from '../utils/bmi-calculator-command';
+import {Observable} from 'rxjs';
+import {BmiCalculatorOutput} from '../utils/bmi-calculator-output';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BmiCalculatorService {
+  private static URL: string = `${environment.API_URL}/${apis.BMICALCULATOR_URL}`;
+
+  constructor(private _httpClient: HttpClient) { }
+
+  public calculBmi(calculBmiCommand: BmiCalculatorCommand): Observable<BmiCalculatorOutput> {
+    console.log(this._httpClient.post<BmiCalculatorOutput>(BmiCalculatorService.URL, calculBmiCommand, {withCredentials: true}));
+    return this._httpClient.post<BmiCalculatorOutput>(BmiCalculatorService.URL, calculBmiCommand, {withCredentials: true});
+  }
+}
