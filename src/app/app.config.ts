9e7dc84a4a7691743bc2dada7d543b37e8ffc9d1
RobinRHELHa <la229732@student.helha.be>
2024-11-20 12:03:13 +0100

    Add echarts library to project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,7 @@ import {routes} from './app.routes';
 import {provideHttpClient} from '@angular/common/http';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {provideToastr} from 'ngx-toastr';
+import {provideEcharts} from 'ngx-echarts';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,6 +13,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(),
     provideAnimations(),
-    provideToastr()
+    provideToastr(),
+    provideEcharts(),
   ]
 };
