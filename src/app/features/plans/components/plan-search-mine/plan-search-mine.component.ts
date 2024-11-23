5f170ed4be4916987074a6b1e0355e8905fd423f
Nathan Malbecq <la227315@student.helha.be>
2024-11-23 17:42:21 +0100

    Added plan-info and change the display of the plans + add "labels" when we show the plans

--- src/app/features/plans/components/plan-search-mine/plan-search-mine.component.ts
+++ src/app/features/plans/components/plan-search-mine/plan-search-mine.component.ts
@@ -3,18 +3,26 @@ import {ReactiveFormsModule} from "@angular/forms";
 import {PlanService} from '../../services/plan.service';
 import {AuthService} from '../../../../core/auth/services/auth.service';
 import {PlanSearchOutput} from '../../utils/plan-search-output';
+import {PlanInfoComponent} from '../plan-info/plan-info.component';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-plan-search-public-mine',
   standalone: true,
-    imports: [
-        ReactiveFormsModule
-    ],
+  imports: [
+    ReactiveFormsModule,
+    PlanInfoComponent,
+    NgClass
+  ],
   templateUrl: './plan-search-mine.component.html',
-  styleUrl: './plan-search-mine.component.css'
+  styleUrls: [
+    './plan-search-mine.component.css',
+    '../../../../shared/styles/plan-table.css',
+  ],
 })
 export class PlanSearchMineComponent implements OnInit{
   plans: PlanSearchOutput[] = [];
+  selectedPlan: any;
 
   constructor(private _planService: PlanService, private _authService: AuthService) {
   }
@@ -28,5 +36,9 @@ export class PlanSearchMineComponent implements OnInit{
         alert(error.message);
       }
     });
+  }
+
+  showPlanInfo(planId: number) {
+    this.selectedPlan = this.plans.find(plan => plan.id === planId);
   }
 }
