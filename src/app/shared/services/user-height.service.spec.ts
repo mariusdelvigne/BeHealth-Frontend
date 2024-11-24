488867a6d62f89b44754d02c6484e42ed36ef8e6
RobinRHELHa <la229732@student.helha.be>
2024-11-24 16:36:23 +0100

    Create user weight service

---
+++ src/app/shared/services/user-height.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { UserHeightService } from './user-height.service';
+
+describe('UserHeightService', () => {
+  let service: UserHeightService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(UserHeightService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
---
+++ src/app/shared/services/user-height.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {apis, environment} from '../../../environments/environment';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {AuthService} from '../../core/auth/services/auth.service';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserHeightService {
+  private static URL: string = `${environment.API_URL}/${apis.USERS_URL}`;
+
+  constructor(private _httpClient: HttpClient, private _authService: AuthService) { }
+
+  public getAllBetween(from: Date, to: Date, pageNumber: number, pageSize: number): Observable<any> {
+    let userId = this._authService.getId();
+
+    let params = new HttpParams()
+      .set('from', from.toISOString())
+      .set('to', to.toISOString())
+      .set('pageNumber', pageNumber)
+      .set('pageSize', pageSize);
+
+    return this._httpClient.get<any>(UserHeightService.URL + `/${userId}/heights`, {
+      params: params,
+      withCredentials: true
+    });
+  }
+}
