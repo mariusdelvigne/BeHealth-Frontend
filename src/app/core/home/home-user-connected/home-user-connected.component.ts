5d492a0dde10b762e16880947e14fc00e92a763a
Marius Delvigne <marius.delvigne@gmail.com>
2024-11-27 10:45:50 +0100

    adding home components if user is connected or not

---
+++ src/app/core/home/home-user-connected/home-user-connected.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-user-connected',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-user-connected.component.html',
+  styleUrl: './home-user-connected.component.css'
+})
+export class HomeUserConnectedComponent {
+
+}
