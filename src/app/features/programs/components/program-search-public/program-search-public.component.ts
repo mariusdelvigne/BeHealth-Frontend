e4c52ef26208783937aed2a4b10c749a98c692ca
Matteo Leroy <matteo.leroy2004@gmail.com>
2024-12-04 12:07:06 +0100

    add error response if relation already exists

--- src/app/features/programs/components/program-search-public/program-search-public.component.ts
+++ src/app/features/programs/components/program-search-public/program-search-public.component.ts
@@ -60,6 +60,7 @@ export class ProgramSearchPublicComponent implements OnInit {
         console.log(response);
       },
       error: error => {
+        this._toastrService.error( relation + " already exists!");
         console.error(error);
       }
     });
