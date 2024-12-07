9fa000b6945b86421ef15096dd2f0b32421ac701
RobinRHELHa <la229732@student.helha.be>
2024-12-07 14:30:11 +0100

    Change weight-graph and its options to use graph-base

--- src/app/features/profile/components/profile/physical-graph/weight-graph/weight-graph.component.ts
+++ src/app/features/profile/components/profile/physical-graph/weight-graph/weight-graph.component.ts
@@ -1,10 +1,11 @@
 import {Component, OnInit} from '@angular/core';
-import {DatedValue} from '../../../../utils/DatedValue';
-import {EChartsOption, SeriesOption} from 'echarts';
 import {NgxEchartsDirective} from 'ngx-echarts';
 import {DatePipe} from '@angular/common';
 import {UserWeightService} from '../../../../../../shared/services/user-weight.service';
-import {firstValueFrom} from 'rxjs';
+import {GraphBase} from '../../graphs/utils/graph-base';
+import {GetWeightGraphOptions} from '../../graphs/utils/WeightGraphOptions';
+import {map, Observable} from 'rxjs';
+import {DatedValue} from '../../../../utils/DatedValue';
 
 @Component({
   selector: 'app-weight-graph',
@@ -16,127 +17,24 @@ import {firstValueFrom} from 'rxjs';
   templateUrl: './weight-graph.component.html',
   styleUrl: './weight-graph.component.css'
 })
-export class WeightGraphComponent implements OnInit {
-  startDate: Date = new Date();
-  endDate: Date = new Date();
-
-  data: DatedValue[] = [];
-
-  options: EChartsOption = {
-    xAxis: {
-      name: 'Time',
-      type: 'time',
-      axisLabel: {
-        formatter: (value: number) => {
-          return `${this._datePipe.transform(value, 'd/M/y')}`;
-        },
-      },
-      nameTextStyle: {
-        fontWeight: 'bold',
-      },
-      min: this.startDate,
-      max: this.endDate,
-      splitNumber: 2,
-    },
-    yAxis: {
-      name: 'Weight in kg',
-      type: 'value',
-      nameTextStyle: {
-        fontWeight: 'bold',
-      },
-      axisLine: {
-        show: true,
-      }
-    },
-    tooltip: {
-      trigger: 'axis',
-      formatter: (params: any) => {
-        const data = params[0].data;
-        return `<div class="text-center">
-                    <div><b>${data[1]} kg</b></div>
-                    <div>${this._datePipe.transform(data[0], 'd/M/y')}</div>
-                </div>`;
-      },
-    },
-    series: {
-      name: 'Weight',
-      type: 'line',
-      data: this.data.map(d => [d.date, d.value]),
-      symbol: 'circle',
-      symbolSize: 10,
-      itemStyle: {
-        color: 'rgba(15, 80, 250, 0.9)',
-      },
-      lineStyle: {
-        color: 'rgba(15, 80, 250, 0.9)',
-        width: 3,
-      },
-      areaStyle: {
-        color: 'rgba(15, 80, 250, 0.3)',
-      },
-    },
-  };
-
-  chart!: NgxEchartsDirective;
-
-  constructor(private _datePipe: DatePipe, private _userWeightService: UserWeightService) { }
-
-  ngOnInit(): void {
-    this.startDate.setDate(1);
-    this.startDate.setHours(0, 0, 0, 0);
-
-    this.endDate = new Date(this.startDate.getFullYear(), this.startDate.getMonth() + 1, 0,
-      23, 59, 59, 999);
+export class WeightGraphComponent extends GraphBase implements OnInit {
+  constructor(datePipe: DatePipe, private _userWeightService: UserWeightService) {
+    super(datePipe);
   }
 
-  onChartInit(e: any): void {
-    this.chart = e;
-    this.loadData();
+  override ngOnInit(): void {
+    super.ngOnInit();
+    this.options = GetWeightGraphOptions(this.startDate, this.endDate, this._datePipe, this.data);
   }
 
-  changeMonth(next: boolean) {
-    this.startDate = new Date(this.startDate
-      .setMonth(this.startDate.getMonth() + (next ? 1 : -1))
+  override loadPageOfData(pageNumber: number, pageSize: number): Observable<DatedValue[]> {
+    let response = this._userWeightService.getAllBetween(this.startDate, this.endDate, pageNumber, pageSize);
+    return response.pipe(
+      map(r => r.userWeights.map((d: { inputDate: string; weightInG: number; }) => ({
+          date: new Date(d.inputDate),
+          value: d.weightInG / 1000
+        })
+      ))
     );
-    this.endDate = new Date(this.startDate.getFullYear(), this.startDate.getMonth() + 1, 0,
-      23, 59, 59, 999);
-
-    this.loadData();
-  }
-
-  async loadData() {
-    this.data = [];
-    let dataToAdd: DatedValue[] = [];
-    let pageNumber = 0;
-    const pageSize: number = 20;
-
-    do {
-      let response = await firstValueFrom(this._userWeightService.getAllBetween(this.startDate, this.endDate, pageNumber++, pageSize));
-      dataToAdd = response.userWeights.map((d: any) => ({
-        date: new Date(d.inputDate),
-        value: d.weightInG / 1000,
-      }));
-
-      this.data = this.data.concat(dataToAdd);
-    } while(dataToAdd.length == pageSize);
-
-    this.updateChart();
-  }
-
-  updateChart() {
-    if (this.chart !== undefined) {
-      console.log(this.data);
-      const series = this.options.series as SeriesOption;
-      series.data = this.data.map(d => [d.date, d.value]);
-
-      const xAxis = this.options.xAxis;
-      // @ts-ignore
-      xAxis!.min = this.startDate;
-      // @ts-ignore
-      xAxis!.max = this.endDate;
-
-      // @ts-ignore
-      this.chart.setOption(this.options);
-    }
   }
 }
