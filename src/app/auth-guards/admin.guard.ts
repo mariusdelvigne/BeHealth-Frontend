d970f16baf6177027129025d10709a1c1f08118e
RobinRHELHa <la229732@student.helha.be>
2024-12-07 19:39:56 +0100

    Fix guards not working

--- src/app/auth-guards/admin.guard.ts
+++ src/app/auth-guards/admin.guard.ts
@@ -1,14 +1,19 @@
 import {CanActivateFn, Router} from '@angular/router';
 import {inject} from '@angular/core';
 import {AuthService} from '../core/auth/services/auth.service';
+import {map} from 'rxjs';
 
 export const adminGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  if (!authService.isAuthenticated() || authService.getRole() !== 'Admin') {
-    router.navigate(['/']);
-    return false;
-  }
-  return true;
+  return authService.loadData().pipe(
+    map(connected => {
+      if (connected && authService.getRole() === 'Admin')
+        return true;
+
+      router.navigate(['/']);
+      return false;
+    })
+  );
 };
