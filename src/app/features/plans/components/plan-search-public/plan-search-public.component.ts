d3b2acc625eea183032860a782b99045a195fa75
Malbecq Nathan <la227315@student.helha.be>
2024-12-08 22:26:57 +0100

    Changed style for plan-public form

--- src/app/features/plans/components/plan-search-public/plan-search-public.component.ts
+++ src/app/features/plans/components/plan-search-public/plan-search-public.component.ts
@@ -19,21 +19,6 @@ import {NgClass} from '@angular/common';
   styleUrls: [
     './plan-search-public.component.scss',
   ],
-  animations: [
-    trigger('colorChange', [
-      state('grey', style({
-        backgroundColor: 'rgba(173, 181, 189, 0.3)',
-        border: '3px solid rgba(173, 181, 189, 0.5)',
-      })),
-      state('blue', style({
-        backgroundColor: 'rgba(13, 110, 253, 0.3)',
-        border: '3px solid rgba(13, 110, 253, 0.5)',
-      })),
-      transition('grey <=> blue', [
-        animate('1s ease-out')
-      ])
-    ])
-  ]
 })
 export class PlanSearchPublicComponent implements OnInit {
   plans: PlanSearchOutput[] = [];
