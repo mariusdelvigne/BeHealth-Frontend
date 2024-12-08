8dc149509affc49884f8c4eccacafb8fdb401662
RobinRHELHa <la229732@student.helha.be>
2024-12-08 14:43:59 +0100

    Change dashboard according to backend data

--- src/app/features/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/components/dashboard/dashboard.component.ts
@@ -4,6 +4,7 @@ import {UserService} from '../../../../shared/services/user.service';
 import {AuthService} from '../../../../core/auth/services/auth.service';
 import {DatePipe} from '@angular/common';
 import {TimePipe} from '../../../../shared/utils/time.pipe';
+import {DashboardData} from '../../DashboardData';
 
 @Component({
   selector: 'app-dashboard',
@@ -17,39 +18,12 @@ import {TimePipe} from '../../../../shared/utils/time.pipe';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent implements OnInit {
-  userData = {
-    sleep: {
-      startDatetime: new Date(2024, 12, 5),
-      endDatetime: new Date(2024, 12, 6),
-    },
-    food: {
-      eatenDatetime: new Date(),
-      quantityInG: 200,
-      name: 'Apple',
-    },
-    sport: {
-      startDatetime: new Date(),
-      endDatetime: new Date(),
-      name: 'Marathon',
-    },
-    period: {
-      startDate: new Date(),
-      endDate: new Date(),
-    },
-    height: {
-      inputDate: new Date(),
-      heightInCm: 0,
-    },
-    weight: {
-      inputDate: new Date(),
-      weightInKg: 0,
-    }
-  };
+  userData: DashboardData = {};
 
   constructor(private _userService: UserService, private _authService: AuthService) { }
 
-  dateDiff(startDate: Date, endDate: Date): number {
-    return (startDate.getTime() - endDate.getTime()) / 1000;
+  dateDiff(startDate: Date | string, endDate: Date | string): number {
+    return (new Date(startDate).getTime() - new Date(endDate).getTime()) / 1000;
   }
 
   ngOnInit(): void {
