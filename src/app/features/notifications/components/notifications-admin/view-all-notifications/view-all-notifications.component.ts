7763ba5351188887ffb4d5c694d98e693e82a236
Marius Delvigne <marius.delvigne@gmail.com>
2024-12-11 16:44:05 +0100

    style for notification

--- src/app/features/notifications/components/notifications-admin/view-all-notifications/view-all-notifications.component.ts
+++ src/app/features/notifications/components/notifications-admin/view-all-notifications/view-all-notifications.component.ts
@@ -1,31 +1,60 @@
 import {Component, OnInit} from '@angular/core';
 import {NotificationsGetAllOutput} from '../../../utils/notifications-get-all-output';
-import {AuthService} from '../../../../../core/auth/services/auth.service';
 import {NotificationService} from '../../../../../shared/services/notification.service';
 import {ToastrService} from 'ngx-toastr';
+import {UserService} from '../../../../../shared/services/user.service';
 
 @Component({
   selector: 'app-view-all-notifications',
   standalone: true,
   imports: [],
   templateUrl: './view-all-notifications.component.html',
-  styleUrl: './view-all-notifications.component.scss'
+  styleUrls: ['./view-all-notifications.component.scss']
 })
 export class ViewAllNotificationsComponent implements OnInit {
   notifications: NotificationsGetAllOutput[] = [];
 
-  constructor(private _authService: AuthService, private _notificationService: NotificationService, private _toastrService: ToastrService) {
+  constructor(private _notificationService: NotificationService, private _toastrService: ToastrService, private _userService: UserService) {
   }
 
   ngOnInit() {
     this._notificationService.getAllNotifications().subscribe({
       next: (notifications) => {
         this.notifications = notifications.notifications;
+
+        this.notifications.forEach((notification, index) => {
+          this.getUserReceiver(notification, index);
+        });
       },
       error: (error) => {
-        console.log(error);
         this._toastrService.error("Error : " + error.message);
       }
-    })
+    });
   }
+
+  private getUserReceiver(notification: NotificationsGetAllOutput, index: number) {
+    const userId = notification.userId;
+    this._userService.getById(userId).subscribe({
+      next: (user) => {
+        this.notifications[index].userName = user.username;
+      },
+      error: (error) => {
+        this._toastrService.error("Error : " + error.message);
+      }
+    });
+  }
+
+  public getDateTime(sendingDateTime: string): string {
+    const date = new Date(sendingDateTime);
+
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes}`;
+  }
+
 }
--- src/app/features/notifications/utils/notifications-get-all-output.ts
+++ src/app/features/notifications/utils/notifications-get-all-output.ts
@@ -1,6 +1,7 @@
 export interface NotificationsGetAllOutput {
   id: number;
   userId: number;
+  userName: string;
   title: string;
   category: string;
   description: string;
