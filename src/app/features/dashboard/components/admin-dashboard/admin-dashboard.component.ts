0a17f886310f2e05dc8373d72bb0be93c0915169
Marius Delvigne <marius.delvigne@gmail.com>
2024-12-11 14:13:06 +0100

    creation of first card dashboard admin

--- src/app/features/dashboard/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/features/dashboard/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {DatePipe} from '@angular/common';
+import {RouterLink} from '@angular/router';
+import {TimePipe} from '../../../../shared/utils/time.pipe';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    DatePipe,
+    RouterLink,
+    TimePipe
+  ],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.scss'
 })
