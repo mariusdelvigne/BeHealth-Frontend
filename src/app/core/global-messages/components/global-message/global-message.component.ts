d80833a3613912698238778a9e40f9033ea1714d
Marius Delvigne <marius.delvigne@gmail.com>
2024-12-13 15:44:11 +0100

    create global message components

---
+++ src/app/core/global-messages/components/global-message/global-message.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-global-message',
+  standalone: true,
+  imports: [],
+  templateUrl: './global-message.component.html',
+  styleUrl: './global-message.component.scss'
+})
+export class GlobalMessageComponent {
+
+}
