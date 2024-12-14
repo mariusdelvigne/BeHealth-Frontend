df1ad60a31bf1275271f419797dc82fb6ca20c53
Malbecq Nathan <la227315@student.helha.be>
2024-12-14 22:31:47 +0100

    Changed styles for Bmi, ... forms

--- src/app/features/nutrition-calculate/components/nutrition-calculator/nutrition-calculator.component.ts
+++ src/app/features/nutrition-calculate/components/nutrition-calculator/nutrition-calculator.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
-import {animate, state, style, transition, trigger} from '@angular/animations';
 import {NutritionCalculatorOutput} from '../../utils/nutrition-calculator-output';
 import {NutritionCalculatorService} from '../../services/nutrition-calculator.service';
 import {ToastrService} from 'ngx-toastr';
@@ -14,21 +13,6 @@ import {ToastrService} from 'ngx-toastr';
   ],
   templateUrl: './nutrition-calculator.component.html',
   styleUrl: './nutrition-calculator.component.scss',
-  animations: [
-    trigger('colorChange', [
-      state('grey', style({
-        backgroundColor: 'rgba(173, 181, 189, 0.3)',
-        border: '3px solid rgba(173, 181, 189, 0.5)',
-      })),
-      state('blue', style({
-        backgroundColor: 'rgba(13, 110, 253, 0.3)',
-        border: '3px solid rgba(13, 110, 253, 0.5)',
-      })),
-      transition('grey <=> blue', [
-        animate('1s ease-out')
-      ])
-    ])
-  ]
 })
 export class NutritionCalculatorComponent {
   form: FormGroup = new FormGroup({
