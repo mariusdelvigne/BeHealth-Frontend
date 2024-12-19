75923f7fbc5366b28bf6808308ca7a975a7ab1e8
Marius Delvigne <marius.delvigne@gmail.com>
2024-12-19 20:00:07 +0100

    Production policy apply

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,18 @@
 export const environment = {
-  API_URL: ""
+  API_URL: "http://localhost:8080/api/v1",
 };
 
 export const apis = {
-  AUTH_URL: "",
-  USERS_URL: "",
-  PLAN_URL: "",
-  FOOD_URL: "",
-  SPORT_URL: "",
-  PERIODS_URL: "",
-  PLANS_URL: "",
-  PROGRAMS_URL: "",
-  BMI_CALCULATOR_URL: "",
-  NUTRITION_CALCULATOR_URL: "",
-  SPORT_CALCULATOR_URL: "",
-  NOTIFICATIONS_URL: "",
-  GLOBAL_MESSAGES_URL: "",
+  AUTH_URL: "sessions",
+  USERS_URL: "users",
+  FOOD_URL: "foods",
+  SPORT_URL: "sports",
+  PERIODS_URL: "periods",
+  PLANS_URL: "plans",
+  PROGRAMS_URL: "programs",
+  BMI_CALCULATOR_URL: "bmi-calculator",
+  NUTRITION_CALCULATOR_URL: "food-calculator",
+  SPORT_CALCULATOR_URL: "sport-calculator",
+  NOTIFICATIONS_URL: "notifications",
+  GLOBAL_MESSAGES_URL: "messages",
 };
